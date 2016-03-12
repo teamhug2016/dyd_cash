47fd1d6629ac8b3d996e1033aa8d607c807ab99f
dyd <dockerify_your_dev@dev.dev>
2016-03-12 19:27:04 +1100

    first commit -- can execute node5

---
+++ cash.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+DEFAULT_IMAGE="node5"
+
+declare -A imagemap
+imagemap[node5]="dev/node5.7"
+
+IMAGE="${1:-$DEFAULT_IMAGE}"
+CONTAINER_NAME="${PWD##*/}.cash_${IMAGE}"
+BUILD_IMAGE=${imagemap[$IMAGE]}
+BUILD_VOLUMES="-v ${PWD}:/data"
+
+function isContainerRunning() {
+  docker ps | grep "$1" | wc -l
+}
+
+isRunning=`isContainerRunning ${CONTAINER_NAME}`
+if [ $isRunning -eq 0 ]; then
+  echo "Running $CONTAINER_NAME first time"
+  docker run -dt \
+    --net=host \
+    --name ${CONTAINER_NAME} \
+    ${BUILD_VOLUMES} \
+    ${BUILD_IMAGE} sh
+fi;
+
+docker exec -it ${CONTAINER_NAME} sh
